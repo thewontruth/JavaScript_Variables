9d7c8ec8a2a73e0279630cdcdd2c28892017270c
thewontruth <thewontruth@gmail.com>
2023-06-14 10:01:18 -0500

    Create Java

--- app.js
+++ app.js
@@ -3,8 +3,37 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Rosie";
+let lastName = "Cotton";
+var age = 51;
 
-// Exercise 2
+lastName = "Gamgee";
+age = 52;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+let product = "T-shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log (
+  `${product} in size ${selectedSize} is $${price} with ${stock} in stock.`
+);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log (
+  `${title} by ${author} is ${pageCount} pages long. Your Last bookmark was page ${bookmark}.`
+);
